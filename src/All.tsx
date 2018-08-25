837a255f171dc32bdc62e61afc9830c28f86f72a
Biel Simon Rojas <bielsimon2@gmail.com>
2018-08-25 16:43:49 +0200

    Fixed internal order of nesting. Now it properly supports components that depend on each other implicitly.

--- src/All.tsx
+++ src/All.tsx
@@ -12,7 +12,7 @@ export interface AllProps {
 export class All extends Component<AllProps> {
 
     render() {
-        const tree: Function = _.reduce(this.props.of, (accumulator: Function, value, index, array) => {
+        const tree: Function = _.reduceRight(this.props.of, (accumulator: Function, value, index, array) => {
             let C;
             let Cprops = {};
             if(_.isArray(value)){
@@ -24,7 +24,7 @@ export class All extends Component<AllProps> {
             return (...params) => {
                 return <C {...this.props.props} {...Cprops} >
                     {(cRender) => {
-                        return accumulator(cRender, ...params)
+                        return accumulator(...params, cRender)
                     }}
                 </C>
             }
